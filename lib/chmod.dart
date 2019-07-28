7f8edcba6c948322920518f27502c51ada832f1f
hunnble <hante_hunter@163.com>
2019-07-28 20:55:10 +0800

    feat: add chmod func, update SDK

--- lib/chmod.dart
+++ lib/chmod.dart
@@ -1,7 +1,23 @@
 library chmod;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'dart:io';
+
+List<String> getArgs(String filename, mode) {
+  if (mode is num) {
+    mode = mode.toString();
+  }
+  if (mode is String) {
+    if (mode.startsWith('0o')) {
+      mode = mode.substring(2);
+    }
+    return [mode, filename];
+  }
+  return [];
+}
+
+Future chmod(String filename, String mode) async {
+  List<String> args = getArgs(filename, mode);
+  if (args.length > 0) {
+    return await Process.run('chmod', args);
+  }
 }
